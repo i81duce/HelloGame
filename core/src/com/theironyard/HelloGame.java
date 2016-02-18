886020ec33a84a50c26a85902e06d0f4fc7d0570
KevinBozic <bozic.kevin@gmail.com>
2016-02-18 10:59:52 -0500

    Do things

--- core/src/com/theironyard/HelloGame.java
+++ core/src/com/theironyard/HelloGame.java
@@ -2,6 +2,7 @@ package com.theironyard;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,19 +10,54 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class HelloGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	float x, y, xv, yv;
+
+	static final float MAX_VELOCITY = 100; // this is a constant
 	
 	@Override
-	public void create () {
+	public void create () { // used to initialize. main method in a sense. This is run once
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render () { // where the magic happens!! This is run many many times
+		move();
+
+		Gdx.gl.glClearColor(1, 0, 0, 1); // clears last frame
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clears last frame
+		batch.begin(); // prepares frame
+		batch.draw(img, x, y); // passes frame to it
+		batch.end(); // ends frame
+	}
+
+	float decelerate(float velocity) {
+		float deceleration = 0.99f; // the closer to 1, the slower the deceleration;
+		velocity *= deceleration;
+		if (Math.abs(yv) < 1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+
+	void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) { // This takes input
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			yv = MAX_VELOCITY * -1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			xv = MAX_VELOCITY * -1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			xv = MAX_VELOCITY;
+		}
+
+		y += yv * Gdx.graphics.getDeltaTime(); // getting a sliver of Max Velocity
+		x += xv * Gdx.graphics.getDeltaTime(); // getting a sliver of Max Velocity
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
 	}
 }
